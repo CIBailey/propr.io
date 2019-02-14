e74c8859ecd5f9fb1f3bec4d1e93da44f9e1b995
Christina Bailey <christinaibailey@live.com>
2019-02-14 12:11:11 +0100

    authentication fixed

--- routes/form-route.js
+++ routes/form-route.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const User = require("../models/user-model.js");
-const Tenant = require("../models/tenant-model.js");
 const Property = require("../models/property-model.js");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const bcryptSalt = 10;
 const fileUploader = require("../config/file-upload.js");
+const checkLandlord = checkRoles("Landlord");
+
+/* Authentition Permissions*/
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  };
+}
 
 /* SIGNUP Page */
 
@@ -116,7 +128,7 @@ router.get("/process-logout", (req, res, next) => {
 });
 
 /*  EDIT PROFILE page */
-router.get("/edit-profile", (req, res, next) => {
+router.get("/edit-profile", checkLandlord, (req, res, next) => {
   res.render("forms/edit-profile.hbs");
 });
 
@@ -178,14 +190,34 @@ router.get("/edit-profile", (req, res, next) => {
 // );
 
 // /*  ADD PROPERTY page */
-router.get("/add-property", (req, res, next) => {
+router.get("/add-property", checkLandlord, (req, res, next) => {
   res.render("forms/add-property.hbs");
 });
 
-// router.post("/process-add-property", fileUploader.single("featurePhoto"),(req, res, next) => {
-//   .then(
-//     res.redirect("/properties");
-//   )
-//   .catch(err => next(err));
+router.post(
+  "/process-add-property",
+  fileUploader.single("featurePhoto"),
+  (req, res, next) => {
+    const { name, description, rentAmount, amenities, address } = req.body;
 
-// });
+    const featurePhoto = req.file.secure_url;
+    console.log(req.body.address);
+    console.log(street1, street2, city, zipcode, country);
+    // { street1, street2, city, zipcode, country }
+    const property = new Property({
+      name: name,
+      description: description,
+      rentAmount: rentAmount,
+      amenities: { amenities },
+      address
+    });
+    console.log(property);
+    property
+      .save()
+      .then(newProperty => {
+        console.log("😎 property created");
+        res.redirect("/properties");
+      })
+      .catch(err => next(err));
+  }
+);
--- routes/list-route.js
+++ routes/list-route.js
@@ -5,20 +5,33 @@ const Property = require("../models/property-model.js");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const fileUploader = require("../config/file-upload.js");
+const checkLandlord = checkRoles("Landlord");
 
-router.get("/properties", (req, res, next) => {
+/* Authentiation Permissions*/
+
+function checkRoles(role) {
+  return function(req, res, next) {
+    if (req.isAuthenticated() && req.user.role === role) {
+      return next();
+    } else {
+      res.redirect("/login");
+    }
+  };
+}
+
+router.get("/properties", checkLandlord, (req, res, next) => {
   res.render("lists/properties.hbs");
 });
 
-router.get("/tenants", (req, res, next) => {
+router.get("/tenants", checkLandlord, (req, res, next) => {
   res.render("lists/tenants.hbs");
 });
 
-router.get("/payments", (req, res, next) => {
+router.get("/payments", checkLandlord, (req, res, next) => {
   res.render("lists/payments.hbs");
 });
 
-router.get("/messages", (req, res, next) => {
+router.get("/messages", checkLandlord, (req, res, next) => {
   res.render("lists/messages.hbs");
 });
 
